840a657115a6167627412d4669153d93d2788253
SeiOu <tvuong1207@gmail.com>
2022-10-18 11:50:18 +0700

    Decorate website and build a responsive website

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './Root.css';
+import './Grid.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+import './Responsive.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Body />
+    <Footer />
   </React.StrictMode>
 );
 
